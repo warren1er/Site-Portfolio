6ca24c9c8617c18ec20859d31b02a8c8319e0d6c
warren1er <kbousardo@gmail.com>
2018-06-27 11:26:10 +0200

    creation du pied de page du site en php

---
+++ php/footer.php
@@ -0,0 +1,9 @@
+<section class="container" id="Contact">
+  <div class="row">
+    <div class="bloc col-md-12 text-center">
+      <!-- lien vers le compte GitHub -->
+      <p>Mon compte GitHub:</p>
+      <a href="https://github.com/warren1er">Kévin BOUSARDO</a>
+    </div>
+  </div>
+</section>

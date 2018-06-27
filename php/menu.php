7dde7d13e2d1fa4a926914564e867e78a064ea3c
warren1er <kbousardo@gmail.com>
2018-06-27 11:23:31 +0200

    creation du menu en php

---
+++ php/menu.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-dark bg-dark menu"> <!--Menu du site  -->
+  <a class="navbar-brand" href="#">Web Morningstar Agency</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#Accueil">Accueil<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#Contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+</nav>
